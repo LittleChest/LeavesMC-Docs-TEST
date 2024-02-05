470d54906e1c10c4d463b433f99c25bf253f8141
MC_XiaoHei <xiaohei.xor7studio@foxmail.com>
2024-02-05 08:52:33 +0800

    refactor: refactor ConfigViewerNode

--- src/components/ConfigViewerNode.vue
+++ src/components/ConfigViewerNode.vue
@@ -17,29 +17,19 @@ const props = defineProps({
       @mouseleave="hover[key] = false"
     >
       <div v-if="value['default'] === undefined">
-        <span
-          :class="{ 'hover-with-child': hover[key] }"
-          style="border-radius: 4px; padding: 0 4px 0 4px"
-        ><span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">{{ key }}</span><span
-          style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">:</span></span
-        >
+        <span style="padding: 0 4px 0 4px">
+          <span :class="hover[key]?'key-text-hover':'key-text'">{{ key }}</span>
+          <span :class="hover[key]?'key-text-hover':'key-text'">:</span>
+        </span>
         <ConfigViewerNode :data="value" />
       </div>
-      <div
-        v-else
-        :class="{ 'hover-without-child': hover[key] }"
-        style="border-radius: 4px; padding: 0 4px 0 4px; white-space: normal"
-      >
-        <span style="--shiki-light:#22863A;--shiki-dark:#85E89D;">{{ key }}</span><span
-        style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">:</span> <span
-        style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;">{{ value.default }}</span>
+      <div v-else style="border-radius: 4px; padding: 0 4px 0 4px; white-space: normal">
+        <span :class="hover[key]?'key-text-hover':'key-text'">{{ key }}</span>
+        <span class="value-text">: </span>
+        <span class="value-text">{{ value.default }}</span>
       </div>
     </div>
-    <div
-      v-else
-      :class="value.type + ` custom-block`"
-      style="margin-right: 16px"
-    >
+    <div v-else :class="value.type + ` custom-block`" style="margin-right: 16px">
       <p class="custom-block-title">
         {{ value.title === undefined ? value.type.toUpperCase() : value.title }}
       </p>
@@ -49,14 +39,21 @@ const props = defineProps({
 </template>
 
 <style scoped>
-.hover-with-child {
-  background-color: var(--vp-c-brand-3);
-  color: white;
+
+.key-text {
+  --shiki-light: #22863a;
+  --shiki-dark: #85e89d;
   transition: all 0.5s;
 }
 
-.hover-without-child {
-  color: var(--vp-c-brand-1);
+.key-text-hover {
+  --shiki-light: #003eb3;
+  --shiki-dark: #69b1ff;
   transition: all 0.5s;
+}
+
+.value-text {
+  --shiki-light: #032f62;
+  --shiki-dark: #9ecbff;
 }
 </style>
