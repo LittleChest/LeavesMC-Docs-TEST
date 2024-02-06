80667102ab6fb50e4bcf59ea4ba13375521bd7b8
Bluemangoo <chenfy2006@qq.com>
2024-02-06 17:57:31 +0800

    feat: enable list value render

--- src/components/ConfigViewerNode.vue
+++ src/components/ConfigViewerNode.vue
@@ -65,14 +65,24 @@ function dynamicValue(value: string): string {
         <ConfigViewerNode :data="value" :padding="true" />
       </div>
       <div v-else style="white-space: pre-wrap" @click="expand[key] = !expand[key]">
-        <span class="line config-line" role="button">
+        <div class="line config-line" role="button">
           <span :class="hover[key] ? 'config-key-text-hover' : 'config-key-text'">{{ key }}</span>
           <span class="config-value-text">: </span>
+          <div
+            v-if="typeof value.default=='object'"
+            v-for="v in value.default" :key="key" style="padding-left: 16px">
+            <span class="config-line">
+              <span class="config-list">- </span>
+              <span
+                :class="isSpecial(v) ? 'config-value-special' : 'config-value-text'"
+              >{{ v }}</span>
+            </span>
+          </div>
           <span
+            v-else
             :class="isSpecial(resolveValue(value)) ? 'config-value-special' : 'config-value-text'"
-            v-html="resolveValue(value)"
-          />
-        </span>
+          >{{ resolveValue(value) }}</span>
+        </div>
         <div
           v-if="expand[key]"
           class="custom-block info inline-block"
@@ -113,6 +123,11 @@ function dynamicValue(value: string): string {
 .config-value-special {
   --shiki-light: #005cc5;
   --shiki-dark: #79b8ff;
+}
+
+.config-list {
+  --shiki-light: #24292E;
+  --shiki-dark: #E1E4E8;
 }
 
 .inline-block {
